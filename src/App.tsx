86009639b4f47a0a228bcb9eadb7aafceb0208f2
taerim.woo <taerim.woo@quantum-ai.ai>
2024-02-02 17:30:24 +0900

    DB연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // const webPush = async (text: any) => {
+  //   const registration = await navigator.serviceWorker.ready;
+  //   console.log(registration);
+  //   registration.showNotification('웹푸쉬', { body: text });
+  // };
+
+  useEffect(() => {
+    getNoty();
+  }, []);
+
+  async function getNoty() {
+    await supabase
+      .channel('*')
+      .on('postgres_changes', { event: '*', schema: '*' }, async (payload) => {
+        if (payload.table === 'noty') {
+          // const data: any = payload.new;
+          if (payload.new) {
+            console.log(payload);
+            // webPush(payload.new.text);
+          }
+        }
+      })
+      .subscribe();
+  }
 
   return (
     <>
@@ -29,7 +60,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
