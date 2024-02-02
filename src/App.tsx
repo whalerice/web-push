dde82628eeb2a38114c006ce9eb62ae1cbbbe793
taerim.woo <taerim.woo@quantum-ai.ai>
2024-02-02 17:59:19 +0900

    웹푸시테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect } from 'react';
+import { supabase } from '@/lib/supabase';
 import './App.css';
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-);
 
 function App() {
-  const [count, setCount] = useState(0);
+  const webPush = async (text: any) => {
+    const registration = await navigator.serviceWorker.ready;
+    console.log(registration);
+    registration.showNotification('웹푸쉬', { body: text });
+  };
 
-  // const webPush = async (text: any) => {
-  //   const registration = await navigator.serviceWorker.ready;
-  //   console.log(registration);
-  //   registration.showNotification('웹푸쉬', { body: text });
-  // };
+  const requestPermission = async () => {
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey:
+        'BCjQjaIt1dH87Qd9iOr42bk-1Eu_s-RGMI4bsCvYGW4qS2m49ZkOb-p9psaYH3ZXYNCNc6bJkC_xQB2jF6gNZxs',
+    });
+    // TODO: DB에 구독 정보 보내기
+    console.log('subscription => ', subscription.toJSON());
+    // const subscription = await registration.pushManager.getSubscription();
+    //   if (subscription) {
+    //     // 이미 구독이 되어있다면 해지하기
+    //     // TODO: DB에 구독 해지 정보 보내기
+    //     subscription.unsubscribe();
+    //   } else {
+    //     // 구독이 되어있지 않으면 구독하기
+    //     const subscription = await registration.pushManager.subscribe({
+    //       userVisibleOnly: true,
+    //       applicationServerKey:
+    //         'BCjQjaIt1dH87Qd9iOr42bk-1Eu_s-RGMI4bsCvYGW4qS2m49ZkOb-p9psaYH3ZXYNCNc6bJkC_xQB2jF6gNZxs',
+    //     });
+    //     // TODO: DB에 구독 정보 보내기
+    //     console.log('subscription => ', subscription.toJSON());
+    //   }
+    // } catch (e: any) {
+    //   console.error(e.message);
+    // }
+  };
 
   useEffect(() => {
     getNoty();
+
+    requestPermission();
   }, []);
 
   async function getNoty() {
@@ -27,10 +49,10 @@ function App() {
       .channel('*')
       .on('postgres_changes', { event: '*', schema: '*' }, async (payload) => {
         if (payload.table === 'noty') {
-          // const data: any = payload.new;
+          const data: any = payload.new;
           if (payload.new) {
             console.log(payload);
-            // webPush(payload.new.text);
+            webPush(data.text);
           }
         }
       })
@@ -39,26 +61,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button>알림 테스트</button>
     </>
   );
 }
